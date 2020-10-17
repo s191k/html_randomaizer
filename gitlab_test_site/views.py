f41287ad437d829b2857d2cb4af9725a562525a7
Kupyrev Alexander <alexander.kupyrev@pladform.ru>
2020-10-17 21:26:54 +0300

    +

--- gitlab_test_site/views.py
+++ gitlab_test_site/views.py
@@ -18,41 +18,41 @@ def index(request):
     print(tasks)
     print(autors)
 
-    if tasks.strip() != '' and autors.strip() != '':
-        res_map, unsigned_tests = randomaizer_def(tasks, autors)
-        res_text = ''
-        #
-        # if tasks.strip() != '' and autors.strip() != '':
-        #     tests_per_person = math.floor(len(tasks) / len(autors))
-        #     tests_left = len(tasks) - tests_per_person * len(autors)
-        #
-        
-
-        tasks = tasks.split('\n')
-        autors = autors.split('\n')
-
-        tests_per_person = math.floor(len(tasks) / len(autors))
-        tests_left = len(tasks) - tests_per_person * len(autors)
-
-        res_text += "Количество тестов :: " + str(len(tasks)) + " \n "
-        res_text += "Количество участников :: " + str(len(autors)) + " \n "
-        res_text += "Количество тестов на человека :: " + str(tests_per_person) + " \n "
-        res_text += "Количество нераспределенных тестов :: " + str(tests_left) + " \n "
+    if tasks is not None and autors is not None:
+        if tasks.strip() != '' and autors.strip() != '':
+            res_map, unsigned_tests = randomaizer_def(tasks, autors)
+            res_text = ''
+            #
+            # if tasks.strip() != '' and autors.strip() != '':
+            #     tests_per_person = math.floor(len(tasks) / len(autors))
+            #     tests_left = len(tasks) - tests_per_person * len(autors)
+            #
 
 
+            tasks = tasks.split('\n')
+            autors = autors.split('\n')
 
-        for key in res_map:
-            res_text += key + " :: \n"
-            for cur_task in res_map[key]:
-                res_text += " -- " + cur_task + "\n"
-            res_text += "---------------------"
+            tests_per_person = math.floor(len(tasks) / len(autors))
+            tests_left = len(tasks) - tests_per_person * len(autors)
 
-        if len(unsigned_tests) > 0:
+            res_text += "Количество тестов :: " + str(len(tasks)) + " \n "
+            res_text += "Количество участников :: " + str(len(autors)) + " \n "
+            res_text += "Количество тестов на человека :: " + str(tests_per_person) + " \n "
+            res_text += "Количество нераспределенных тестов :: " + str(tests_left) + " \n "
 
-            res_text += "---Нераскиданные тесты---\n"
-            for cur_task in unsigned_tests:
-                res_text += cur_task
 
-        return render(request, 'gitlab_test_site/htmlpage.html', {'result':res_text} )
+            for key in res_map:
+                res_text += key + " :: \n"
+                for cur_task in res_map[key]:
+                    res_text += " -- " + cur_task + "\n"
+                res_text += "---------------------"
+
+            if len(unsigned_tests) > 0:
+
+                res_text += "---Нераскиданные тесты---\n"
+                for cur_task in unsigned_tests:
+                    res_text += cur_task
+
+            return render(request, 'gitlab_test_site/htmlpage.html', {'result':res_text} )
     else:
         return render(request, 'gitlab_test_site/htmlpage.html', {'result':''})

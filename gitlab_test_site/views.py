43e2c1410e897a5e4b8cbef71f3b47240a417fa8
Kupyrev Alexander <alexander.kupyrev@pladform.ru>
2020-10-18 12:49:34 +0300

    upd

--- gitlab_test_site/views.py
+++ gitlab_test_site/views.py
@@ -1,10 +1,4 @@
-import re
-
 import math
-from django.http import HttpResponse
-from bs4 import BeautifulSoup
-import requests
-import configs
 from django.shortcuts import render
 
 from randomaizer import randomaizer_def
@@ -16,32 +10,43 @@ def index(request):
 
 
     if tasks is not None and autors is not None:
-        print(tasks is not None and autors is not None)
         if tasks.strip() != '' and autors.strip() != '':
-            print(tasks.strip() != '' and autors.strip() != '')
+
+            tasks = list(map(lambda x: x.replace('\n','').replace('\r',''), tasks.split('\n')))
+            autors = list(map(lambda x: x.replace('\n','').replace('\r',''), autors.split('\n')))
+
             res_map, unsigned_tests = randomaizer_def(tasks, autors)
             res_text = ''
 
 
-            tasks = tasks.split('\n')
-            autors = autors.split('\n')
             tests_per_person = math.floor(len(tasks) / len(autors))
             tests_left = len(tasks) - tests_per_person * len(autors)
 
-            res_text += "Количество тестов :: " + str(len(tasks)) + " \n "
-            res_text += "Количество участников :: " + str(len(autors)) + " \n "
-            res_text += "Количество тестов на человека :: " + str(tests_per_person) + " \n "
-            res_text += "Количество нераспределенных тестов :: " + str(tests_left) + " \n "
+            res_text += "Количество тестов :: " + str(len(tasks)) + "\n"
+            res_text += "Количество участников :: " + str(len(autors)) + "\n"
+            res_text += "Количество тестов на человека :: " + str(tests_per_person) + "\n"
+            res_text += "Количество нераспределенных тестов :: " + str(tests_left) + "\n"
+
+            # print(tasks)
+            # print(autors)
 
 
             for key in res_map:
                 res_text += key + " :: \n"
                 for cur_task in res_map[key]:
                     res_text += " -- " + cur_task + "\n"
-                res_text += "---------------------"
+                res_text += "---------------------\n"
+
+            # for key in res_map:
+            #     for cur_task in res_map[key]:
+            #         # print(key)
+            #         # print(cur_task)
+            #         print(cur_task.replace("\n",'') + " " + key + "\n")
+            #         res_text += cur_task.replace("\n",'') + " " + key + "\n"
+
+            # print(res_text)
 
             if len(unsigned_tests) > 0:
-
                 res_text += "---Нераскиданные тесты---\n"
                 for cur_task in unsigned_tests:
                     res_text += cur_task
--- randomaizer.py
+++ randomaizer.py
@@ -1,40 +1,13 @@
-import io
 import math
-from random import random, randint
-#
-# with io.open("task.txt", encoding='utf-8') as file:
-#     for line in file:
-#         tasks = file.readlines()
-#     tasks = list(map(lambda x: x[:-1] if x.endswith('\n') else x, tasks))
-#
-# with io.open("autor.txt", encoding='utf-8') as file:
-#     for line in file:
-#         autors = file.readlines()
-#     autors = list(map(lambda x: x[:-1] if x.endswith('\n') else x, autors))
-
+from random import random
 
 def randomaizer_def(tasks,autors):
-
-    print(autors)
-    print(type(autors))
-    print(tasks)
-    print(type(tasks))
-
-    tasks = tasks.split('\n')
-    autors = autors.split('\n')
-
     res_map = {}
     for autor in autors:
         res_map[autor] = []
 
     tests_per_person = math.floor(len(tasks) / len(autors))
     tests_left = len(tasks) - tests_per_person * len(autors)
-
-    print("Количество тестов :: ",len(tasks))
-    print("Количество участников :: ",len(autors))
-    print("Количество тестов на человека :: ",tests_per_person)
-    print("Количество нераспределенных тестов :: ",tests_left)
-    print("---------------------")
 
     unsigned_tests = []
 
@@ -53,16 +26,4 @@ def randomaizer_def(tasks,autors):
     for key in res_map.keys():
         result_taks.extend(res_map[key])
 
-    for key in res_map:
-        print(key + " :: \n")
-        for cur_task in res_map[key]:
-            print(" -- " + cur_task)
-        print("---------------------")
-
-    if tests_left > 0:
-        print("---Нераскиданные тесты---")
-        print(unsigned_tests)
-
     return res_map,unsigned_tests
-    # for key in res_map:
-    #     print(key + " :: " + str(len(res_map[key])))
